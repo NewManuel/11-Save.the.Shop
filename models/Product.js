aeb37210d15bfa193ab4b484c0f2361dcf306744
NewManuel <100541053+NewManuel@users.noreply.github.com>
2024-04-21 20:04:11 -0500

    File - the product.js file has logic description added

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,13 @@
+//THe below variables define and utilizes the Sequelize library for object-relational mapping (ORM) to interact with a SQL database. 
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-
+//The Product class extends the Model class provided by Sequelize.
 class Product extends Model { }
 
 Product.init(
-  {
-  
+  {  
+    //Each attribute of the product model is defined with its data type, constraints(such as allowNull, primaryKey), and validations(such as isDecimal, isNumeric).
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -41,6 +42,7 @@ Product.init(
     },
   },
   {
+    //The sequelize object is passed to specify the connection to the database, options like timestamps, freezeTableName, underscored, and modelName are provided to configure the behavior of the model in Sequelize.
     sequelize,
     timestamps: false,
     freezeTableName: true,
@@ -48,5 +50,5 @@ Product.init(
     modelName: "product",
   }
 );
-
+//Below the module.exports statement exports the Product model, allowing it to be imported and used in other parts of the application.
 module.exports = Product;
