c01b08f2e122904f93060a31c89580415d1c5725
NewManuel <100541053+NewManuel@users.noreply.github.com>
2024-04-21 19:11:11 -0500

    File - tag-routes.js logic description is added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,6 +1,8 @@
+//Require Dependencies - The file requires the necessary dependencies, including Express and models representing Tags, Products, and Product Tags.
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
+//GET '/': Retrieves all tags from the database along with associated products. 
 router.get('/', async (req, res) => {
   try {
     const tagData = await Tag.findAll({
@@ -12,8 +14,7 @@ router.get('/', async (req, res) => {
     console.log(err);
   }
 });
-
-
+// GET '/:id': Retrieves a specific tag by its ID from the database along with associated products.
 router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
@@ -28,6 +29,7 @@ router.get('/:id', async (req, res) => {
     res.status(500).json({ message: "Tag is not found" });
   };
 });
+// POST '/': Creates a new tag based on the request body.
 router.post('/', async (req, res) => {
   try {
     const tagData = await Tag.create(req.body);
@@ -36,6 +38,7 @@ router.post('/', async (req, res) => {
     res.status(400).json({ message: "Tag creation failed" });
   }
 });
+// PUT '/:id': Updates an existing tag with the specified ID based on the request body.
 router.put('/:id', async (req, res) => {
   try {
     const tagData = await Tag.create(req.body);
@@ -44,7 +47,8 @@ router.put('/:id', async (req, res) => {
     res.status(400).json({ message: "The tag creation was failed " });
   }
 });
-
+//DELETE '/:id': Deletes a tag with the specified ID from the database.
+//Each route uses asynchronous functions to handle requests. Inside the try block, the relevant operation (e.g., fetching, creating, updating, or deleting) is performed using Sequelize ORM methods. If successful, the server responds with the appropriate HTTP status code and JSON data. If an error occurs, the server responds with an error message and the corresponding HTTP status code.  
 router.delete('/:id', async (req, res) => {
   try {
     const deleted = await Tag.destroy({ where: { id: req.params.id } });
@@ -57,4 +61,5 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+//The router object containing all defined routes is exported to be used in the main Express application file.
 module.exports = router;
