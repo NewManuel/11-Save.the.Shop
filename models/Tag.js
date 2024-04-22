97ca9372dcecc91e31b1490e42b8af4c851b0a28
NewManuel <100541053+NewManuel@users.noreply.github.com>
2024-04-21 19:42:25 -0500

    File - tag.js logic description added

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,14 @@
+//Below lines of code imports the necessary modules from Sequelize and the connection configuration file.
 const { Model, DataTypes } = require("sequelize");
-
 const sequelize = require("../config/connection.js");
 
+// The Tag class extends the Sequelize Model class.
 class Tag extends Model { }
 
+//Inside the Tag class definition, the init() method is used to define the columns of the Tag model.
 Tag.init(
   {
-    // define columns
+    //Two columns are defined: "id" and "tag_name".The "id" column is of type INTEGER, is not allowed to be null, is the primary key, and auto- increments.The "tag_name" column is of type STRING.
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -15,10 +17,10 @@ Tag.init(
     },
 
     tag_name: {
-      type: DataTypes.STRING, ///Set the data type to a STRING
-    },
+      type: DataTypes.STRING,
   },
   {
+    //The init() method also receives an options object where the sequelize property is set to the sequelize instance, indicating the database connection. Other options like timestamps, freezeTableName, underscored, and modelName are also provided.
     sequelize,
     timestamps: false,
     freezeTableName: true,
@@ -27,4 +29,5 @@ Tag.init(
   }
 );
 
+//the Tag model is exported from the file for use in other parts of the application.
 module.exports = Tag;
