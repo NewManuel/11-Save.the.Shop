76655ec2717e1d8e090e4cc6e737f18aea58180a
NewManuel <100541053+NewManuel@users.noreply.github.com>
2024-04-21 20:45:52 -0500

    File - connection.js  has the description for the logic added

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,9 @@
+//require("dotenv").config();: This line loads the dotenv module and configures it to read environment variables from a .env file in the root directory of the project. This allows sensitive configuration data to be stored in environment variables rather than hardcoding them into the code.
 require("dotenv").config();
+
+//const Sequelize = require("sequelize");: This line imports the Sequelize library, which is an Object-Relational Mapping (ORM) tool for Node.js, used for interacting with SQL databases.
 const Sequelize = require("sequelize");
+//const sequelize = process.env.JAWSDB_URL ? ... : ...;: This line initializes a Sequelize instance named sequelize.It checks if the JAWSDB_URL environment variable is set.If it is set, it uses the provided URL to connect to the database.If not, it falls back to using other environment variables (DB_NAME, DB_USER, and DB_PW) to connect to a local MySQL database.
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
@@ -10,4 +14,5 @@ const sequelize = process.env.JAWSDB_URL
     },
   });
 
+//module.exports = sequelize; - This line exports the configured Sequelize instance(sequelize) so that it can be imported and used in other files within the project.
 module.exports = sequelize;
